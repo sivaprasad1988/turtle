b19a68e37ad45348cd7ee52fd725f3c001f09d55
Siva <s.sisupalan@gingco.de>
2024-11-18 21:22:27 +0100

    [TASK] Implement square
    Tasks:
    * Implement square

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from turtle import  Turtle,Screen
 turtle = Turtle()
 turtle.shape("turtle")
 turtle.color('red')
-turtle.rt(90)
-turtle.forward(100)
-
+i = 0
+while i < 4:
+    turtle.forward(100)
+    turtle.rt(90)
+    i +=1
 
 screen = Screen()
 screen.exitonclick()
