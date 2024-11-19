6165f1969951195afb5c0420db0859693d371905
Siva <siva@webofficeit.com>
2024-11-19 16:14:34 +0100

    [TASK] Implement doted lines
    Tasks:
    * Implement doted lines

--- main_1.py
+++ main_1.py
@@ -5,10 +5,11 @@ turtle.shape("turtle")
 turtle.color('red')
 i = 0
 # Draw a square
-for _ in range(4):
-    turtle.forward(100)
-    turtle.rt(90)
-    i += 1
+for _ in range(100):
+    turtle.forward(10)
+    turtle.penup()
+    turtle.forward(10)
+    turtle.pendown()
 
 screen = Screen()
 screen.exitonclick()
