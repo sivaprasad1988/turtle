c3ad5dcdab7352d13e24539a8e177e39adde2617
Siva <siva@webofficeit.com>
2025-01-12 15:22:57 +0100

    [TASK] Add input keypress
    Tasks:
    * Add input keypress

--- Turtle-day-19.py
+++ Turtle-day-19.py
@@ -4,10 +4,25 @@ turtle = Turtle()
 screen = Screen()
 
 
-def move_forward():
+def move():
     turtle.forward(10)
+def back():
+    turtle.backward(10)
+def left():
+    turtle.right(10)
+def right():
+    turtle.left(10)
+def clear():
+    turtle.clear()
+    turtle.penup()
+    turtle.home()
+    turtle.pendown()
 
 
 screen.listen()
-screen.onkey(key="space", fun=move_forward)
+screen.onkey(key="w", fun=move)
+screen.onkey(key="s", fun=back)
+screen.onkey(key="a", fun=left)
+screen.onkey(key="d", fun=left)
+screen.onkey(key="c", fun=clear)
 screen.exitonclick()
